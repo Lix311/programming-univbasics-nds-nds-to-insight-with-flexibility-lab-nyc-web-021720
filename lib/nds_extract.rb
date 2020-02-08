b0cd46b43c1252630d34f0549b71f02915c638b0
Alex Sterrantino <alexsterrantino@gmail.com>
2020-02-08 20:13:28 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -51,7 +51,7 @@ def movies_with_director_key(name, movies_collection)
   movie_aoh = []
   index = 0 
     while index < movies_collection.length 
-      movie_aoh << {movie_with_director_name(name,movies_collection)}
+      movie_aoh << movie_with_director_name(name,movies_collection)
     index += 1 
     binding.pry 
     end 
