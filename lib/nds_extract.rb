7e21b24d0c912fc6c90ac08d98873dd62bc18f9c
Alex Sterrantino <alexsterrantino@gmail.com>
2020-02-08 20:22:58 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,9 +49,8 @@ def movies_with_director_key(name, movies_collection)
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
   movie_aoh = []
-  movie_aoh << movie_with_director_name(name,movies_collection)
-  puts movie_aoh
-  return movie_aoh  
+  return movie_with_director_name(name,movies_collection)
+   
 end
 
 
