6c22fda76c5184159198b478ee6b7f9747151ca8
Alex Sterrantino <alexsterrantino@gmail.com>
2020-02-08 20:43:09 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -84,6 +84,13 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+  movie_aoa = []
+  index = 0 
+  while index < source.length 
+    movie_aoa << source[index][:title]
+    index += 1 
+  end 
+return movie_aoa      
 end
 
 # ----------------    End of Your Code Region --------------------
